2edc23c37843f688f87bc97ec84aedd896b5bf81
AwesomeRidley <raidenbradley06@gmail.com>
2025-03-24 13:35:10 -0800

    Update Project.cpp

--- Project.cpp
+++ Project.cpp
@@ -2,10 +2,17 @@
 
 using namespace std;
 
+void HangmanGame(int game);
+
 int main()
 {
     cout << "Hello, World!" << endl;
     cout<<"1 2 3 4 5"<< endl;
     return 0;
+
+}
+
+void HangmanGame(int game){
+
 
 }
